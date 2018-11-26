c022aacfbbc891ba1fdf145177fb1d7100e1fbd6
sjx <1104611441@qq.com>
2018-11-26 13:32:41 +0800

    lstm first version finished

--- LSTM/lstm.py
+++ LSTM/lstm.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import pandas as pd
+import random
 import os
 import time
 import numpy as np
@@ -43,12 +44,13 @@ def getData(shareCode, featureNum=10, dayNum=500):
     sample_num = len(close_prices) - featureNum
     rdarray = np.random.permutation(range(sample_num))
     maxp = max(close_prices)
-
+    # last = close_prices[0]
     for index in rdarray:
       first = close_prices[index]
       data.append([[(x/first-1)]
                    for x in close_prices[index: index + featureNum]])
       label.append(close_prices[index + featureNum]/first-1)
+      # last = close_prices[index]
       # if close_prices[index + featureNum] >= close_prices[index + featureNum-1]:
       #   label.append(1)
       # else:
@@ -65,12 +67,13 @@ def getData(shareCode, featureNum=10, dayNum=500):
   maxp = max(close_prices)
   # validateLabel里面是处理后的price,我需要用raw_label记录下原始的price
   # raw_label = []
-
+  # last = close_prices[0]
   for index in range(len(close_prices)-featureNum):
     first = close_prices[index]
     validateData.append([[(x/first-1)]
                          for x in close_prices[index: index + featureNum]])
     validateLabel.append(close_prices[index + featureNum]/first-1)
+    # last = close_prices[index]
     # raw_label.append(close_prices[index + featureNum])
     # if close_prices[index + featureNum] >= close_prices[index + featureNum-1]:
     #   validateLabel.append(1)
@@ -84,15 +87,12 @@ def getData(shareCode, featureNum=10, dayNum=500):
 sess = tf.Session()
 
 featureNum = 10
+dayNum = 500
 # LSTM 输入是三维，[样本数, timestep, 单个时间片的维度]
 data, label, validateData, validateLabel, close_prices, validateFirsts = getData(
-    '000625', featureNum, 500)
+    '000002', featureNum, dayNum)
+# 预测结果长度是dayNum-featureNum
 
-# Data = [[[(i+j)/100] for i in range(5)] for j in range(100)]
-# target = [(i+10)/100 for i in range(100)]
-
-# data = np.array(Data, dtype=float)
-# target = np.array(target, dtype=float)
 
 # x_train, x_test, y_train, y_test = train_test_split(data, target, test_size=0.2, random_state=4)
 x_train = np.array(data, dtype=float)
@@ -107,7 +107,13 @@ model = Sequential()
 model.add(LSTM((50), batch_input_shape=(None, 10, 1), return_sequences=True))
 model.add(Dropout(0.2))
 
-model.add(LSTM((100), return_sequences=False))
+model.add(LSTM((50), return_sequences=True))
+model.add(Dropout(0.2))
+
+model.add(LSTM((50), return_sequences=True))
+model.add(Dropout(0.2))
+
+model.add(LSTM((50), return_sequences=False))
 model.add(Dropout(0.2))
 
 model.add(Dense(1))
@@ -123,7 +129,7 @@ print('compilation time : ', time.time()-start)
 #########
 # train #
 #########
-history = model.fit(x_train, y_train, batch_size=32, epochs=3, validation_data=(x_test, y_test))
+history = model.fit(x_train, y_train, batch_size=32, epochs=10, validation_data=(x_test, y_test))
 
 ###########
 # predict #
@@ -153,10 +159,60 @@ accracyArray = np.array(accracyArray)
 accuracy = accracyArray.sum()/len(accracyArray)
 print('accuracy: ', accuracy)
 
+###################################
+# calculate double moving average #
+###################################
+# 用了今天的收盘价 以后需要改成收盘前10min的价格
+def calAvgArray(prices, dayNum):
+  length = len(prices) - dayNum
+
+  avgArray = [None]*length
+  # print('!!!!!!!!',self.startIndex)
+  priceSum = 0
+  for i in range(dayNum):
+    priceSum = priceSum + prices[i]
+
+  for day in range(length):
+    priceSum = priceSum - prices[day] + prices[day+dayNum]
+    avgArray[day] = priceSum/dayNum
+  
+  return avgArray
+
+# prices要多传几天数据,最后结果长度需要490，所以要传500天
+shortAvg = calAvgArray(close_prices[-493:], 3)
+longAvg = calAvgArray(close_prices[-500:], 10)
+
+state = 1
+upStateArray = [] # 短 > 长
+downStateArray = [] # 长 > 短
+for i in range(1,len(close_prices)):
+  if state > 0:
+    if close_prices[i-1] > close_prices[i]:
+      upStateArray.append(state)
+      state = -1
+    else:
+      state = state+1
+  elif state < 0:
+    if close_prices[i-1] < close_prices[i]:
+      downStateArray.append(-state)
+      state = 1
+    else:
+      state = state-1
+
+upStateArray = np.array(upStateArray)
+downStateArray = np.array(downStateArray)
+
+plt.figure()
+plt.subplot(211)
+plt.hist(upStateArray, 8, normed=True)
+plt.subplot(212)
+plt.hist(downStateArray, 8, normed=True)
+plt.show()
+
 #################################
 # calculate profit and drawback #
 #################################
-def evaluate(prediction, label, close_prices, featureNum):
+def evaluate(prediction, label, close_prices, longAvg, shortAvg, featureNum):
   capital = 100000
   oldCapital = capital
 
@@ -166,6 +222,8 @@ def evaluate(prediction, label, close_prices, featureNum):
   # trade_history.append('keep out')
 
   isIn = 0
+  state = 1
+
   for i in range(1,len(prediction)):
     # i+featureNum-1代表今天
     # 判断盈亏
@@ -179,22 +237,72 @@ def evaluate(prediction, label, close_prices, featureNum):
       else:
         profitRateArray.append(profitRateArray[-1])
 
-    # 买卖
-    if prediction[i] > label[i-1] and isIn == 0:
-      isIn = 1
-      trade_history.append('in')
-      inPrice = close_prices[i+featureNum-1]
-    elif prediction[i] < label[i-1] and isIn == 1:
-      isIn = 0
-      trade_history.append('out')
-      oldCapital = oldCapital/inPrice * close_prices[i+featureNum-1]
-    else:
-      if isIn == 0:
-        trade_history.append('keep out')
-      elif isIn == 1:
-        trade_history.append('keep in')
+    # # 买卖
+    # if prediction[i] > label[i-1] and prediction[i] > 0 and isIn == 0:
+    #   isIn = 1
+    #   trade_history.append('in')
+    #   inPrice = close_prices[i+featureNum-1]
+    # elif (prediction[i] < label[i-1] or prediction[i] < 0) and isIn == 1:
+    #   isIn = 0
+    #   trade_history.append('out')
+    #   oldCapital = oldCapital/inPrice * close_prices[i+featureNum-1]
+    # else:
+    #   if isIn == 0:
+    #     trade_history.append('keep out')
+    #   elif isIn == 1:
+    #     trade_history.append('keep in')
 
-  
+    if state > 0:
+      if longAvg[i-1] > shortAvg[i-1]:
+        state = -1
+      else:
+        state = state+1
+    else:
+      if longAvg[i-1] < shortAvg[i-1]:
+        state = state-1
+      else:
+        state = 1
+
+    # 买卖
+    if isIn == 0: # 未持有股票
+      if prediction[i] > label[i-1] and prediction[i] > 0: # 判断明天会涨
+        isIn = 1
+        trade_history.append('in')
+        inPrice = close_prices[i+featureNum-1]
+      else: # 判断明天不会涨
+        # trade_history.append('keep out')
+        # 以一定概率买入
+        if state < 0:
+          probability = sum(downStateArray < -state)/len(downStateArray)
+          buy = random.random() < probability**4
+          if buy == 1:
+            isIn = 1
+            trade_history.append('in')
+            inPrice = close_prices[i+featureNum-1]
+          else:
+            trade_history.append('keep out')
+        else:
+          trade_history.append('keep out')
+    else: # 持有股票
+      if prediction[i] < label[i-1] or prediction[i] < 0: # 判断明天会跌
+        isIn = 0
+        trade_history.append('out')
+        oldCapital = oldCapital/inPrice * close_prices[i+featureNum-1]
+      else: # 判断明天不会跌
+        # trade_history.append('keep in')
+        # 以一定概率卖出
+        if state > 0:
+          probability = sum(upStateArray > state)/len(upStateArray)
+          sell = random.random() > probability
+        
+          if sell == 1:
+            isIn = 0
+            trade_history.append('out')
+            oldCapital = oldCapital/inPrice * close_prices[i+featureNum-1]
+          else:
+            trade_history.append('keep in')
+        else:
+          trade_history.append('keep in')
 
   max_draw_back = 0
   minI = 0
@@ -232,13 +340,13 @@ def evaluate(prediction, label, close_prices, featureNum):
 
 
 profitRateArray, minI, minJ, trade_history = evaluate(
-    results, y_test, close_prices, featureNum)
+    results, y_test, close_prices, longAvg, shortAvg, featureNum)
 
 ################
 # save results #
 ################
-df = pd.DataFrame(data=np.stack((y_test, results, accracyArray, trade_history, close_prices[-490:], profitRateArray)).T,
-                  columns=['label', 'prediction', 'accuracy', 'trade_history', 'close_prices', 'profitRateArray'])
+df = pd.DataFrame(data=np.stack((y_test, results, accracyArray, trade_history, close_prices[-490:], profitRateArray, longAvg, shortAvg)).T,
+                  columns=['label', 'prediction', 'accuracy', 'trade_history', 'close_prices', 'profitRateArray', 'longAvg', 'shortAvg'])
 df.to_csv('prediciton.csv')
 
 
@@ -261,5 +369,7 @@ plt.scatter(minJ, profitRateArray[minJ], c='r', marker='.')
 plt.legend()
 plt.subplot(414)
 plt.plot(range(len(close_prices)), close_prices, label='price')
+plt.plot(range(len(longAvg)), longAvg, label='longAvg')
+plt.plot(range(len(shortAvg)), shortAvg, label='shortAvg')
 plt.legend()
 plt.show()
